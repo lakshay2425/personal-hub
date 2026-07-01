946f2e71ec2e87f975d1da744d16917c36e407dd
Lakshay <lakshay12290@gmail.com>
2026-07-01 17:09:52 +0530

    chore: delete auth, and axios related files

--- app/lib/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL || '',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
